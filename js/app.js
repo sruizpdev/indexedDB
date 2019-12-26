3fc5f296f1edf5dbb31ded9c1f925cbb76ac1ee5
Sergio Ruiz <sergiotto@outlook.com>
2019-12-26 11:35:29 +0100

    Creacion de la BBDD

--- js/app.js
+++ js/app.js
@@ -1,12 +1,33 @@
 let DB;
 
-const form = document.querySelector('form'),
-      nombreMascota = document.querySelector('#mascota'),
-      nombreCliente = document.querySelector('#cliente'),
-      telefono = document.querySelector('#telefono'),
-      fecha = document.querySelector('#fecha'),
-      hora = document.querySelector('#hora'),
-      sintomas = document.querySelector('#sintomas'),
-      citas = document.querySelector('#citas'),
-      headingAdministra = document.querySelector('#administra');
-console.log(nombreCliente);
+// selectores de la interfaz
+const form = document.querySelector("form"),
+  nombreMascota = document.querySelector("#mascota"),
+  nombreCliente = document.querySelector("#cliente"),
+  telefono = document.querySelector("#telefono"),
+  fecha = document.querySelector("#fecha"),
+  hora = document.querySelector("#hora"),
+  sintomas = document.querySelector("#sintomas"),
+  citas = document.querySelector("#citas"),
+  headingAdministra = document.querySelector("#administra");
+
+// esperamos al DOM ready
+document.addEventListener("DOMContentLoaded", () => {
+  let crearDB = window.indexedDB.open("citas", 1);
+
+  crearDB.onerror = function() {
+    console.log("Hubo un error");
+  };
+
+  crearDB.onsuccess = function() {
+    DB = crearDB.result;
+  };
+
+  crearDB.onupgradeneeded = function(e){
+      let db = e.target.result;
+      console.log(db);
+      
+  }
+
+
+});
