4dd1990ad06aa803f58a65f03aa0302790f6730a
Sergio Ruiz <sergiotto@outlook.com>
2019-12-26 11:18:45 +0100

    Esqueleto de form

---
+++ js/app.js
@@ -0,0 +1,12 @@
+let DB;
+
+const form = document.querySelector('form'),
+      nombreMascota = document.querySelector('#mascota'),
+      nombreCliente = document.querySelector('#cliente'),
+      telefono = document.querySelector('#telefono'),
+      fecha = document.querySelector('#fecha'),
+      hora = document.querySelector('#hora'),
+      sintomas = document.querySelector('#sintomas'),
+      citas = document.querySelector('#citas'),
+      headingAdministra = document.querySelector('#administra');
+console.log(nombreCliente);
