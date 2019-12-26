0c7749d410eb3b0cd7d200ee8378108c69fc4f8a
Sergio Ruiz <sergiotto@outlook.com>
2019-12-26 16:47:47 +0100

    Creando el schema de la BBDD

--- js/app.js
+++ js/app.js
@@ -22,12 +22,48 @@ document.addEventListener("DOMContentLoaded", () => {
   crearDB.onsuccess = function() {
     DB = crearDB.result;
   };
+  // este método sólo corre una vez
+  crearDB.onupgradeneeded = function(e) {
+    // el evento es la misma bbdd
+    let db = e.target.result;
+    // definimos el objectstore
+    let objectStore = db.createObjectStore("citas", {
+      keyPath: "key",
+      autoIncrement: true
+    });
+    objectStore.createIndex("mascota", "mascota", { unique: false });
+    objectStore.createIndex("cliente", "mascota", { unique: false });
+    objectStore.createIndex("telefono", "telefono", { unique: false });
+    objectStore.createIndex("fecha", "fecha", { unique: false });
+    objectStore.createIndex("hora", "hora", { unique: false });
+    objectStore.createIndex("sintomas", "sintomas", { unique: false });
+    console.log("Base de datos creada y lista");
+  };
+  // Cuando se envía el formulario
+  form.addEventListener("submit", agregarDatos);
 
-  crearDB.onupgradeneeded = function(e){
-      let db = e.target.result;
-      console.log(db);
-      
+  function agregarDatos(e) {
+    e.preventDefault();
+    const nuevaCita = {
+      mascota: nombreMascota.value,
+      cliente: nombreMascota.value,
+      telefono: telefono.value,
+      fecha: fecha.value,
+      hora: hora.value,
+      sintomas: sintomas.value
+    };
+    let transaction = DB.transaction(["citas"], "readwrite");
+    let objectStore = transaction.objectStore("citas");
+    let peticion = objectStore.add(nuevaCita);
+    peticion.onsuccess = () => {
+      form.reset();
+    };
+    transaction.oncomplete = () => {
+      console.log("Cita agregada");
+    };
+    transaction.onerror = () => {
+      console.log("HUBO UN ERROR");
+    };
+    console.log(peticion);
   }
-
-
 });
